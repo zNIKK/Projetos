6ef20855550830be08ffffeeacec44fec32b5267
zNIKK <nicolas.gandolfi11@gmail.com>
2022-07-04 23:39:30 -0300

    working Jogo_da_Memoria

--- 001-JS_Projects/Jogo_da_Memoria/scripts/interface.js
+++ 001-JS_Projects/Jogo_da_Memoria/scripts/interface.js
@@ -1,26 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
+const FRONT = "card_front"
+const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
-    let squares = document.querySelectorAll(".square")
+let techs = ['bootstrap',
+'css',
+'electron',
+'firebase', 
+'html', 
+'javascript',
+'jquery',
+'mongo',
+'node',
+'react'];
 
-    squares.forEach((square) =>{
-        square.addEventListener('click', click)
+let cards = null;
+
+startGame();
+  
+function startGame() {
+    initiliazeCards(game.createCardsFromTechs())
+}
+
+
+
+function initiliazeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+
+    game.cards.forEach(card =>{
+        let cardElement = document.createElement('div');
+
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);''
     })
+
+}
     
-})
-
-function click(event) {
-    console.log(event.target);
-    updateImages();
+function flipCard() {
+    this.classList.add("flip")
 }
 
-function updateImages() {
-
-    let squares = document.querySelectorAll(".square")
-
-    squares.forEach((square) => {
-        square.toggleAttribute('special')
-        
-        // square.innerHTML = `<img src="../Jogo_da_Memoria/images/bootstrap.png"></img>`
-        
-    })
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement)
+    createCardFace(BACK, card, cardElement)
 }
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT) {
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = "/Projetos/001-JS_Projects/Jogo_da_Memoria/images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = '&lt/&gt';
+    }
+    element.appendChild(cardElementFace);
+}
+
