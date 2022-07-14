bd1ab7b727155bb672e9c80336cffe057076c959
nikk <nicolas.gandolfi11@gmail.com>
2022-07-13 22:34:10 -0300

    working in sistem of popup message

--- 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/buttons_max_min.js
+++ 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/buttons_max_min.js
@@ -1,12 +1,27 @@
-let cont = 0
+
 
 $("#min_b").click(() => {
-    cont--
+    let cont = $("#amount").val()
+
+    if (cont > 0){
+        cont--
+    } else {
+        cont = 0;
+    }
     $("#amount").val(cont);
 
     })
 
 $("#max_b").click(() => {
-    cont++
+    let cont = $("#amount").val()
+
+    if (cont >= 0) {
+        cont++
+    } else {
+        cont = 0;
+    }
     $("#amount").val(cont);
+
 })
+
+
