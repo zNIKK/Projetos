78282a43ef1764e4cd9ff978691fe3b56b65424b
nikk <nicolas.gandolfi11@gmail.com>
2022-07-16 19:59:22 -0300

    thumbnails finished

--- 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/switch_images.js
+++ 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/switch_images.js
@@ -1,14 +1,42 @@
 
 let thumb = $(".thumbnail").get();
 a = []
+let count_images = 1
+
+
 thumb.forEach(element => {
     $(element).click(() => {
+        let id = $(element).attr("id")
         $(element).children().toggleClass("on")
-        console.log($(element).attr("class"));
-
+        $(element).children().attr("class");
+        $(".main_image").attr("src", src = "images/image-product-"+ id +".jpg" );
+        console.log($(".main_image").attr("src"));
+        // $(".main_image").html($(element).html()) 
     })
-    console.log($(element).attr("class"));
 
 });
+
+$(".thumbnail").children().click(() => {
+    $(".thumbnail").children().removeClass("on")
+
+})
+
+$(".button_left").click(() => {
+    if (count_images > 1){
+        count_images--
+    }
+    
+    $(".main_image").attr("src", src = "images/image-product-"+ count_images +".jpg" );
+
+})
+
+$(".button_right").click(() => {
+    if (count_images < 4){
+        count_images++
+    }
+
+    $(".main_image").attr("src", src = "images/image-product-"+ count_images +".jpg");
+
+})
 
     
