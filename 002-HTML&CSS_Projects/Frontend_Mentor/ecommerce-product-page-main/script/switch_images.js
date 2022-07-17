a0e8b3da28ff81d78fe8df07ad9a5baba0a345ed
zNIKK <nicolas.gandolfi11@gmail.com>
2022-07-17 02:35:46 -0300

    git pull

--- 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/switch_images.js
+++ 002-HTML&CSS_Projects/Frontend_Mentor/ecommerce-product-page-main/script/switch_images.js
@@ -4,10 +4,9 @@ a = []
 thumb.forEach(element => {
     $(element).click(() => {
         $(element).children().toggleClass("on")
-        console.log($(element).attr("class"));
 
     })
-    console.log($(element).attr("class"));
+    console.log($(element).children().attr("class"));
 
 });
 
