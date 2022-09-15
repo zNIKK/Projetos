a3df27aa2da4a9b6ecf34ec7e7e3540ef9409992
NIkk <nicolas.gandolfi11@gmail.com>
2022-09-15 00:31:58 -0300

    learning api rest

--- 001-JS_Projects/Mural_de_avisos/public/scripts/script.js
+++ 001-JS_Projects/Mural_de_avisos/public/scripts/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     updatePost()
 })
+
 function updatePost() {
     fetch("http://192.168.15.28:3000/api/all").then(res => {
         return res.json();
@@ -8,24 +9,27 @@ function updatePost() {
 
         let postElements = "";
         let posts = JSON.parse(json);
+        
         posts.forEach(post => {
             let postElement =
                 `
-            <div id="${post.id}" class="post card mb-4">
+            <div id="${post.id}" id="btn" class="card mb-4">
                 <div class="card-header">
                     <h5 class="card-title">${post.title}</h5>
                 </div>
                 <div class="d-flex card-body justify-content-between">
                     <div class="card-text">${post.description}</div>
-                    <button onclick="deletePost()" class="btn btn-danger">Delete</button>
+                    <button onclick="deletePost('${post.id}')" class="btn btn-danger">Delete</button>
                 </div>
             </div>
             `
             postElements += postElement;
-        });
 
+        });
+        
         document.getElementById("posts").innerHTML = postElements;
     })
+
 }
 
 function newPost() {
@@ -41,21 +45,21 @@ function newPost() {
         body: JSON.stringify(post)
     }
 
-    fetch("http://192.168.15.28:3000/api/new", options).then(res => {
+    fetch("http://192.168.15.28:3000/api/new/", options).then(res => {
         console.log(res);
         updatePost();
     })
 
 }
 
-function deletePost() {
-
+function deletePost(id) {
+        console.log(id);
         const options = {
             method: "DELETE",
             headers: new Headers({ "content-type": "application/json" }),
         }
 
-        fetch(`http://192.168.15.28:3000/api/del/${a}`, options).then(res => {
+        fetch(`http://192.168.15.28:3000/api/del/${id}`, options).then(res => {
             console.log(res);
             updatePost();
         })
