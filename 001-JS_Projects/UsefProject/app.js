9e8e20283d600150153816825b1df0657fa99f68
nikk <nicolas.gandolfi11@gmail.com>
2022-10-28 12:38:09 -0300

    integração do joi no user project

--- 001-JS_Projects/UsefProject/app.js
+++ 001-JS_Projects/UsefProject/app.js
@@ -1,11 +1,21 @@
-const express = require("express");
-const app = express();
-const path = require("path")
-const userRouter = require("./routes/userRoutes")
-
 require("dotenv").config();
 PORT = process.env.PORT;
 HOST = process.env.HOST;
+MONGO_URL = process.env.MONGO_URL;
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const path = require("path")
+const userRouter = require("./routes/userRoutes")
+
+mongoose.connect(MONGO_URL, (err)=> {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Mongo server Connected");
+    }
+})
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "public"));
--- 001-JS_Projects/UsefProject/routes/userRoutes.js
+++ 001-JS_Projects/UsefProject/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const userController = require("../controller/userController.js")
 router.get("/", (req, res) => { res.render("main") })
 
-router.get("/login", (req, res) => { res.render("login") })
-router.get("/register", (req, res) => { res.render("register") })
+router.get("/login", (req, res) => { userController.login })
+router.get("/register", (req, res) => { usercontroller.register })
 
 
 module.exports = router
